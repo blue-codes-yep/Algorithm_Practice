17d4e007100a8a3e1e066a58b7aa39c1dcf30383
Blue Lindsey <linzblue5@gmail.com>
2020-02-28 10:09:30 -0500

    Frequncy pattern exercises.

---
+++ frequency_pattern.py
@@ -0,0 +1,52 @@
+# Exercise 1 ---
+
+# list1 = [1,2,3,4]
+# list2 = [4,3,2,1]
+# list3 = [1,1,5,2]
+
+# def some_func(x,y):
+#     newdic = {}
+#     newx = sorted(x)
+#     newy = sorted(y)
+#     newdic["a"] = newx
+#     newdic["b"] = newy
+#     if newdic["a"] == newdic["b"]:
+#         return True
+#     else:
+#         return False
+
+# print(some_func(list1,list2))
+# print(some_func(list1,list3))
+
+# Exercise 2 ----
+
+# list1 = [1,2,3,4]
+# list2 = [5,4,3,1]
+# list3 = [1,4,9,16]
+
+# def some_func(x,y):
+#     newdic = {}
+#     ans = False
+#     newdic["a"] = x
+#     newdic["b"] = y
+    
+#     for i in newdic["a"]:
+#         if i**2 in newdic["b"]:
+#             ans = True
+#         else: 
+#             ans = False
+#             break
+#     return ans
+# print(some_func(list1, list3))
+# print(some_func(list1, list2))
+
+# Exercise 3 - Not sure if this bad boy works
+# a = "pie"
+# b= "eip"
+# c = "pies"
+
+
+# def some_func(x,y):
+#     newdic = {}
+#     newdic["a"] = x
+#     newdic["b"] = y
